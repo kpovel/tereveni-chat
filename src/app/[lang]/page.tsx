cb77cd09bcf88d7c9ee83ed7aeb0f06bb19dc794
Pavlo Karas <kar.pavel123@gmail.com>
2023-12-02 22:00:18 +0200

    feat: add dictionaries

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,7 +1,11 @@
-export default function Home({params}: {params: {lang: string}}) {
-  return (
-    <div>
-      selected lang is {params.lang}
-      </div>
-  );
+import { getDictionary } from "./dictionaries";
+
+export default async function Home({
+  params,
+}: {
+  params: { lang: "uk" | "en" };
+}) {
+  const dict = await getDictionary(`/${params.lang}`);
+
+  return <h1>{dict.title}</h1>;
 }
