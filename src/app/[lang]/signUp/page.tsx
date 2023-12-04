39e95928d3b185a3da895263d42276da2b7b46a1
Pavlo Karas <kar.pavel123@gmail.com>
2023-12-04 15:34:50 +0200

    fix: escape `'` character

--- src/app/[lang]/signUp/page.tsx
+++ src/app/[lang]/signUp/page.tsx
@@ -1,12 +1,14 @@
-import SignUpForm from './signUpForm';
+import SignUpForm from "./signUpForm";
 
 export default function SignUp() {
-    return (
-        <div className="container mx-auto px-6">
-            <h2 className="mt-20 text-center text-neutral-50 text-lg font-medium ">Hi there! Let's create your account</h2>
-            <div className="mt-10">
-                <SignUpForm />
-            </div>
-        </div>
-    )
+  return (
+    <div className="container mx-auto px-6">
+      <h2 className="mt-20 text-center text-lg font-medium text-neutral-50">
+        Hi there! Let&apos;s create your account
+      </h2>
+      <div className="mt-10">
+        <SignUpForm />
+      </div>
+    </div>
+  );
 }
