5b663cb6b717bfd08fad292b30fdbdd0239deb3a
Pavlo Karas <kar.pavel123@gmail.com>
2023-12-05 11:21:09 +0200

    refactor: update getDictionary data types to improve autocompletion

--- src/app/[lang]/dictionaries.ts
+++ src/app/[lang]/dictionaries.ts
@@ -5,6 +5,12 @@ const dictionaries = {
   "/uk": () => import("./uk.json").then((module) => module.default),
 };
 
-export async function getDictionary(page: keyof typeof dictionaries) {
-  return dictionaries[page]();
+type DictionaryReturnTypes = {
+  [K in keyof typeof dictionaries]: ReturnType<(typeof dictionaries)[K]>;
+};
+
+export async function getDictionary<T extends keyof typeof dictionaries>(
+  page: T,
+): Promise<DictionaryReturnTypes[T]> {
+  return dictionaries[page]() as Promise<DictionaryReturnTypes[T]>;
 }
