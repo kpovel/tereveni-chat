d6a666cbe6456ab05f0d09451612ef58c2d758ff
Pavlo Karas <kar.pavel123@gmail.com>
2023-12-13 15:03:50 +0200

    feat: create onboarding page for choosing categories

--- src/app/[lang]/dictionaries.ts
+++ src/app/[lang]/dictionaries.ts
@@ -5,6 +5,10 @@ const dictionaries = {
   "/uk": () => import("./uk.json").then((module) => module.default),
   "/en/login": () => import("./login/en.json").then((module) => module.default),
   "/uk/login": () => import("./login/uk.json").then((module) => module.default),
+  "/en/onboarding/categories": () =>
+    import("./onboarding/categories/en.json").then((module) => module.default),
+  "/uk/onboarding/categories": () =>
+    import("./onboarding/categories/uk.json").then((module) => module.default),
 };
 
 type DictionaryReturnTypes = {
