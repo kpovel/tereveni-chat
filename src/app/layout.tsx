a96f2301dc061a8fbfb077a4817f816f6ce29378
Pavlo Karas <kar.pavel123@gmail.com>
2023-12-19 21:09:39 +0200

    feat: Add Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -15,6 +16,7 @@ export default function RootLayout({
     <>
       {children}
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
