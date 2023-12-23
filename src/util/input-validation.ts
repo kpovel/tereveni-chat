3b6a8ede47ac877a4f3b571f2d81f2e9cb11f11a
Pavlo Karas <kar.pavel123@gmail.com>
2023-12-23 17:18:16 +0200

    refactor: remove unused code

--- src/util/input-validation.ts
+++ src/util/input-validation.ts
@@ -1,20 +1,5 @@
 import { z } from "zod";
 
-export const validateInput = (
-  inputText: string,
-  type: "password" | "login" | "email",
-) => {
-  const patterns = {
-    password: /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,72}$/,
-    login: /^[A-Za-z0-9]{3,50}$/,
-    email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-  };
-
-  const isValid = patterns[type].test(inputText);
-
-  return isValid;
-};
-
 export function isValidLogin(login: string) {
   const loginSchema = z
     .string()
