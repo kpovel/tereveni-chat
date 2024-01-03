e7ff18047df18bb1151406b67bb0b99db6d8d5d4
Pavlo Karas <kar.pavel123@gmail.com>
2024-01-03 21:28:22 +0200

    fix: type error

--- src/app/[lang]/onboarding/pick-avatar/avatarPost.ts
+++ src/app/[lang]/onboarding/pick-avatar/avatarPost.ts
@@ -1,39 +1,36 @@
-"use server"
+"use server";
 
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { env } from "@/env.mjs";
 
 type SignUpResponseError = {
-    fieldName: string
-    fieldMessage: string
-  };
+  fieldName: string;
+  fieldMessage: string;
+};
 
-export async function avatarPost(
-    formData: FormData,
-    lang: "en" | "uk",) {
+export async function avatarPost(formData: FormData, lang: "en" | "uk") {
+  const accessToken = cookies().get("jwtAccessToken");
 
-    const accessToken = cookies().get("jwtAccessToken").value;
+  if (!accessToken) {
+    redirect("/en");
+  }
 
-    if(!accessToken) {
-        console.log('no access token') 
-    }
+  const res = await fetch(`${env.SERVER_URL}/api/user/avatar/upload`, {
+    method: "POST",
+    body: formData,
+    headers: {
+      Authorization: `Bearer ${accessToken.value}`,
+    },
+    cache: "no-store",
+  });
 
-    const res = await fetch(`${env.SERVER_URL}/api/user/avatar/upload`, {
-        method: "POST",
-        body: formData,
-        headers: {
-            "Authorization": `Bearer ${accessToken}`,
-          },
-        //   cache: "no-store",
-      });
+  if (res.ok) {
+    console.log(res);
+    redirect(`/${lang}/onboarding/introduce-yourself`);
+  }
 
-      if (res.ok) {
-        console.log(res)
-        redirect(`/${lang}/onboarding/introduce-yourself`);
-      }
-    
-      const body = await res.json() as SignUpResponseError;
-    
-      return body.fieldMessage;
+  const body = (await res.json()) as SignUpResponseError;
+
+  return body.fieldMessage;
 }
