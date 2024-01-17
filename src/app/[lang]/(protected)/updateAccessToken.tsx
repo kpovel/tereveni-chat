d797b8149928b34fe31e2377f971edba760dbb25
Pavlo Karas <kar.pavel123@gmail.com>
2024-01-17 14:43:08 +0200

    fix: forgot to save the file

--- src/app/[lang]/(protected)/updateAccessToken.tsx
+++ src/app/[lang]/(protected)/updateAccessToken.tsx
@@ -2,14 +2,11 @@
 
 import { useEffect } from "react";
 import { regenerateAccessToken } from "./regenerateAccessToken";
-import { useParams } from "next/navigation";
 
 export function RegenerateAccessToken() {
-  const params = useParams<{ lang: "en" | "uk" }>();
-
   useEffect(() => {
-    regenerateAccessToken(params.lang);
-  }, [params.lang]);
+    regenerateAccessToken();
+  }, []);
 
   return <></>;
 }
