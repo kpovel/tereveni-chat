f66e7d03e1ee00889ec264c1557f92372cec9c14
Pavlo Karas <kar.pavel123@gmail.com>
2024-01-18 19:23:40 +0200

    would it fix INVALID_IMAGE_OPTIMIZE_REQUEST?

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import "./src/env.mjs";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true
+  }
+};
 
 export default nextConfig;
