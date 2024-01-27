37c1d01010b313a7703042ffed0a392ec3704b13
Pavlo Karas <kar.pavel123@gmail.com>
2024-01-27 21:43:20 +0200

    feat: add translation for cookies-policy page

--- src/app/[lang]/cookies-policy/page.tsx
+++ src/app/[lang]/cookies-policy/page.tsx
@@ -1,11 +1,20 @@
 import { ReactNode } from "react";
 import EnCookiePolicy from "./en.mdx";
+import UkCookiePolicy from "./uk.mdx";
 
 export default function CookiesPolicy({
   params,
 }: {
   params: { lang: "en" | "uk" };
 }) {
+  if (params.lang === "uk") {
+    return (
+      <InfoLayout>
+        <UkCookiePolicy />
+      </InfoLayout>
+    );
+  }
+
   return (
     <InfoLayout>
       <EnCookiePolicy />
@@ -16,9 +25,7 @@ export default function CookiesPolicy({
 function InfoLayout({ children }: { children: ReactNode }) {
   return (
     <main className="w-full px-6 pb-10 pt-[78px]">
-      <div className="prose mx-auto items-stretch text-white">
-        {children}
-      </div>
+      <div className="prose mx-auto items-stretch text-white">{children}</div>
     </main>
   );
 }
