ab103c55452d1cb643b28192c142c2524a13b8da
Pavlo Karas <kar.pavel123@gmail.com>
2024-02-08 11:48:48 +0200

    fix: update layout paddings

--- src/app/util/infoLayout.tsx
+++ src/app/util/infoLayout.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from "react";
 
 export default function InfoLayout({ children }: { children: ReactNode }) {
-    return (
-        <main className="w-full px-6 pt-[40px]">
-            <div className="prose mx-auto items-stretch text-white">
-                {children}
-            </div>
-        </main>
-    )
+  return (
+    <main className="w-full px-6 pb-[40px] pt-[78px]">
+      <div className="prose mx-auto items-stretch text-white">{children}</div>
+    </main>
+  );
 }
