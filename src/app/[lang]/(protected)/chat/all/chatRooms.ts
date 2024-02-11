2fd2c1ea910bef3039a4ca5141f3920c4f29904e
Pavlo Karas <kar.pavel123@gmail.com>
2024-02-11 22:17:42 +0200

    feat: create MessageContainer component

---
+++ src/app/[lang]/(protected)/chat/all/chatRooms.ts
@@ -0,0 +1,49 @@
+import { env } from "@/env.mjs";
+import { getJwtAccessToken } from "../../regenerateAccessToken";
+
+export type ChatRoom = {
+  chatName: string;
+  chatRoom: {
+    uuid: string;
+    description: string;
+    image: null | { name: string };
+  };
+  lastMessage: null | { uuid: string; content: string; dateOfCreated: string };
+};
+
+type ChatRoomResult = {
+  ok: ChatRoom[] | undefined;
+  err: string | undefined;
+};
+
+export async function getChatRooms(): Promise<ChatRoomResult> {
+  const jwtAccessToken = await getJwtAccessToken();
+  const res = await fetchChatRooms(jwtAccessToken);
+
+  if (res.status === 200) {
+    return {
+      ok: await res.json(),
+      err: undefined,
+    } satisfies ChatRoomResult;
+  }
+
+  if (res.status === 401) {
+    return {
+      ok: undefined,
+      err: "Unauthorized",
+    };
+  }
+
+  return {
+    ok: undefined,
+    err: "Failed to fetch chat rooms",
+  };
+}
+
+async function fetchChatRooms(jwtAccessToken: string) {
+  return await fetch(`${env.SERVER_URL}/api/user/chat-rooms`, {
+    headers: {
+      Authorization: `Bearer ${jwtAccessToken}`,
+    },
+  });
+}
--- src/app/[lang]/(protected)/chat/all/page.tsx
+++ src/app/[lang]/(protected)/chat/all/page.tsx
@@ -1,7 +1,24 @@
-export default function AllChats({
-  params,
-}: {
-  params: { lang: "en" | "uk" };
-}) {
-  return <div>All chats</div>;
+import { MessageContainer } from "@/components/chat/MessageContainer";
+import { getChatRooms } from "./chatRooms";
+
+export default async function AllChats({ params }: { params: { lang: Lang } }) {
+  const chatRooms = await getChatRooms();
+
+  if (chatRooms.err) {
+    return <div>{chatRooms.err}</div>;
+  }
+
+  return (
+    <div className="flex flex-col gap-5 px-6">
+      {chatRooms.ok?.map((chatRoom) => {
+        return (
+          <MessageContainer
+            key={chatRoom.chatRoom.uuid}
+            chatRoom={chatRoom}
+            lang={params.lang}
+          />
+        );
+      })}
+    </div>
+  );
 }
