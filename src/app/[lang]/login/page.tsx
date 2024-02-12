af96b1598eb54d415c1ecd3248d973b1fb086e1e
Pavlo Karas <kar.pavel123@gmail.com>
2024-02-12 06:21:57 +0200

    fix: prevent scrolling on the login page when a cookie popup is displayed

--- src/app/[lang]/login/page.tsx
+++ src/app/[lang]/login/page.tsx
@@ -10,13 +10,11 @@ export default async function LoginPage({
   const dict = await getDictionary(`/${params.lang}/login`);
 
   return (
-    <div className="container mx-auto px-6">
-      <h2 className="mt-20 text-center text-lg font-medium text-neutral-50">
+    <div className="container mx-auto flex max-w-md flex-col items-center gap-10 px-6 py-10">
+      <h2 className="text-center text-lg font-medium text-neutral-50">
         {dict.welcomeBack}
       </h2>
-      <div className="mx-auto mt-10 max-w-md">
-        <LoginForm lang={params.lang} dict={dict} />
-      </div>
+      <LoginForm lang={params.lang} dict={dict} />
       <CookiePopup lang={params.lang} />
     </div>
   );
