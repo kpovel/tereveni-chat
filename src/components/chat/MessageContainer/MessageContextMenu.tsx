82b9d78c1b5283047fc07dc8941f14986f0dd9a0
Pavlo Karas <kar.pavel123@gmail.com>
2024-02-12 16:52:59 +0200

    fix: prevent the "Delete chat" button from being transparent

--- src/components/chat/MessageContainer/MessageContextMenu.tsx
+++ src/components/chat/MessageContainer/MessageContextMenu.tsx
@@ -35,23 +35,20 @@ export function MessageContextMenu({
   }
 
   return (
-    <div
-      className="relative"
-      id={chatId}
-      onContextMenu={openContextMenu}
-      ref={ref}
-    >
+    <div id={chatId} onContextMenu={openContextMenu} ref={ref}>
       {children}
-      <button
-        id={messageContextId}
-        onClick={deleteChat}
-        className="absolute right-0 bottom-[-54px] flex hidden w-[164px] cursor-pointer
-        gap-2 rounded-lg bg-[#1F1F1F] px-4 py-[15px]
-        shadow-[0px_8px_10px_1px_rgba(0,_0,_0,_0.12)]"
-      >
-        <TrashIcon />
-        Delete chat
-      </button>
+      <div className="relative">
+        <button
+          id={messageContextId}
+          onClick={deleteChat}
+          className="absolute right-0 top-1 flex hidden w-[164px] gap-2
+          rounded-lg bg-[#1F1F1F] px-4 py-[15px]
+          shadow-[0px_8px_10px_1px_rgba(0,_0,_0,_0.12)]"
+        >
+          <TrashIcon />
+          Delete chat
+        </button>
+      </div>
     </div>
   );
 }
