1a00c1130b31a4caf8ff0da07c53e99627c77138
Pavlo Karas <kar.pavel123@gmail.com>
2024-02-14 06:43:37 +0200

    fix: display the not found page

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,8 +1,14 @@
+import "../globals.css";
+import type { Metadata } from "next";
 import { ReactNode } from "react";
 import { Mulish } from "next/font/google";
 import { SetPreferredLanguage } from "./setPreferredLang";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+
+export const metadata: Metadata = {
+  title: "Create Next App",
+};
 
 const poppins = Mulish({
   weight: ["200", "300", "400", "500", "600", "700"],
--- src/app/route.ts
+++ src/app/route.ts
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
-export default async function Home() {
+export function GET() {
   redirect("/en");
 }
