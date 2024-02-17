548b522511418debecc4381ede318f9c58c7e7b9
Pavlo Karas <kar.pavel123@gmail.com>
2024-02-17 18:11:30 +0200

    fix: if selected language isn't supported set en as a default

--- src/app/[lang]/setPreferredLang.tsx
+++ src/app/[lang]/setPreferredLang.tsx
@@ -6,6 +6,10 @@ import { setPreferredLang } from "./preferredLang";
 export function SetPreferredLanguage({ lang }: { lang: string }) {
   useEffect(() => {
     (async () => {
+      if (lang !== "en" && lang !== "uk") {
+        await setPreferredLang("en");
+      }
+
       await setPreferredLang(lang);
     })();
   }, [lang]);
