3ae65fb7f34b84a9ca7f0681abc3f94611ac1ec9
Pavlo Karas <kpovel5@gmail.com>
2024-02-23 15:15:09 +0200

    feat: blur outer chats on context action

---
+++ src/app/[lang]/(protected)/chat/all/chatList.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { MessageContainer } from "@/components/chat/MessageContainer";
+import { ChatRoom } from "./chatRooms";
+import { useState } from "react";
+
+export function ChatList({
+  chatList,
+  lang,
+}: {
+  chatList: ChatRoom[];
+  lang: Lang;
+}) {
+  const [selectedChat, setSelectedChat] = useState("");
+
+  return (
+    <>
+      {chatList.map((chatRoom) => {
+        return (
+          <MessageContainer
+            key={chatRoom.chatRoom.uuid}
+            chatRoom={chatRoom}
+            lang={lang}
+            setSelectedChat={setSelectedChat}
+            selectedChat={selectedChat}
+          />
+        );
+      })}
+    </>
+  );
+}
--- src/components/chat/MessageContainer/index.tsx
+++ src/components/chat/MessageContainer/index.tsx
@@ -2,17 +2,46 @@ import { ChatRoom } from "@/app/[lang]/(protected)/chat/all/chatRooms";
 import Link from "next/link";
 import { ChatAvatar } from "./ChatAvatar";
 import { ChatInfo } from "./ChatInfo";
+import { Dispatch, SetStateAction, useRef, MouseEvent, useEffect } from "react";
 
 export function MessageContainer({
   chatRoom,
   lang,
+  setSelectedChat,
+  selectedChat,
 }: {
   chatRoom: ChatRoom;
   lang: Lang;
+  setSelectedChat: Dispatch<SetStateAction<string>>;
+  selectedChat: string;
 }) {
+  const ref = useRef<HTMLAnchorElement>(null);
+
+  function handleContextMenu(e: MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault();
+    setSelectedChat(chatRoom.chatRoom.uuid);
+  }
+
+  useEffect(() => {
+    if (selectedChat === "") {
+      ref.current?.classList.remove("blur-sm");
+      return;
+    }
+
+    if (selectedChat === chatRoom.chatRoom.uuid) {
+      ref.current?.classList.remove("blur-sm");
+      return;
+    }
+
+    ref.current?.classList.add("blur-sm");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedChat]);
+
   return (
     <Link
       href={`/${lang}/chat/${chatRoom.chatRoom.uuid}`}
+      ref={ref}
+      onContextMenu={handleContextMenu}
       className="flex gap-3 rounded-2xl bg-[rgba(255,_255,_255,_0.05)] p-3
         shadow-[0px_8px_10px_1px_rgba(0,_0,_0,_0.12)]"
     >
@@ -33,4 +62,3 @@ export function MessageContainer({
     </Link>
   );
 }
-
