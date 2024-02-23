c1577227f190ac8bdf3befa33f53f5abf2b74620
Pavlo Karas <kpovel5@gmail.com>
2024-02-23 06:50:45 +0200

    fix: remove error messages on focus

--- src/components/input/ConfirmPasswordInput.tsx
+++ src/components/input/ConfirmPasswordInput.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import lock from "public/lock.svg";
 import { TogglePassword } from "./PasswordInput";
+import { ErrorMessage } from "./internal/errorMessage";
 
 export function ConfigmPasswordInput({
   placeholder,
@@ -11,20 +12,26 @@ export function ConfigmPasswordInput({
   errorMessage: string;
 }) {
   const [isHidden, setIsHidden] = useState(true);
+  const [showError, setShowError] = useState(false);
   const ref = useRef<HTMLInputElement>(null);
   const redBorder = "border-[#FF453A]";
 
   useEffect(() => {
+    setShowError(true);
     const passwordInput = document.getElementsByName("password")[0];
 
     if (errorMessage) {
       ref.current?.classList.add(redBorder);
       passwordInput.classList.add(redBorder);
     }
-  });
+  }, [errorMessage]);
 
-  function removeRedBorder() {
+  function handleOnBlur() {
     ref.current?.classList.remove(redBorder);
+  }
+
+  function handleOnFocus() {
+    setShowError(false);
   }
 
   return (
@@ -33,7 +40,8 @@ export function ConfigmPasswordInput({
         <Image src={lock} alt="lock" className="absolute left-5" />
         <input
           ref={ref}
-          onBlur={removeRedBorder}
+          onBlur={handleOnBlur}
+          onFocus={handleOnFocus}
           className="w-full rounded-3xl border border-[#444] bg-[#1F1F1F] py-3.5
           pl-14 pr-5 leading-normal outline-none transition ease-in
           autofill:filter-none invalid:border-[#FF453A] focus:border-[#7C01F6]"
@@ -43,11 +51,9 @@ export function ConfigmPasswordInput({
         />
         <TogglePassword isHidden={isHidden} setIsHidden={setIsHidden} />
       </label>
-      {errorMessage && (
-        <div className="whitespace-pre-line px-2 pt-1 text-xs text-[#FF453A]">
-          {errorMessage}
-        </div>
-      )}
+      <div className="whitespace-pre-line text-balance px-2 pt-1 text-xs">
+        <ErrorMessage showError={showError} errorMessage={errorMessage} />
+      </div>
     </div>
   );
 }
--- src/components/input/EmailInput.tsx
+++ src/components/input/EmailInput.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import mailLogo from "public/mail.svg";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { ErrorMessage } from "./internal/errorMessage";
 
 export function EmailInput({
   placeholder,
@@ -9,17 +10,23 @@ export function EmailInput({
   placeholder: string;
   errorMessage: string;
 }) {
+  const [showError, setShowError] = useState(false);
   const ref = useRef<HTMLInputElement>(null);
   const redBorder = "border-[#FF453A]";
 
   useEffect(() => {
+    setShowError(true);
     if (errorMessage) {
       ref.current?.classList.add(redBorder);
     }
-  });
+  }, [errorMessage]);
 
-  function removeRedBorder() {
+  function handleOnBlur() {
     ref.current?.classList.remove(redBorder);
+  }
+
+  function handleOnFocus() {
+    setShowError(false);
   }
 
   return (
@@ -28,7 +35,8 @@ export function EmailInput({
         <Image src={mailLogo} alt="Mail logo" className="absolute left-5" />
         <input
           ref={ref}
-          onBlur={removeRedBorder}
+          onBlur={handleOnBlur}
+          onFocus={handleOnFocus}
           className="w-full rounded-3xl border border-[#444] bg-[#1F1F1F] py-3.5
           pl-14 pr-5 leading-normal outline-none transition ease-in
           autofill:filter-none invalid:border-[#FF453A] focus:border-[#7C01F6]"
@@ -37,9 +45,9 @@ export function EmailInput({
           placeholder={placeholder}
         />
       </label>
-      {errorMessage && (
-        <div className="px-2 pt-1 text-xs text-[#FF453A]">{errorMessage}</div>
-      )}
+      <div className="px-2 pt-1 text-xs text-balance whitespace-pre-line">
+        <ErrorMessage showError={showError} errorMessage={errorMessage} />
+      </div>
     </div>
   );
 }
