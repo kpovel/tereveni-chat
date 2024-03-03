33de137ba433a4ba3b5c848180d107729f2e62ee
Pavlo Karas <thepavlokaras@gmail.com>
2024-03-03 20:50:47 +0200

    fix: center links

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -12,23 +12,21 @@ export default async function Home({
   const dict = await getDictionary(`/${params.lang}`);
 
   return (
-    <div>
-      <div className="container mx-auto px-6">
-        <ToggleLang />
-        <div className="inline-flex w-full items-center justify-center px-4 py-20">
-          <Image src={tereveniLogo} alt="Tereveni Logo" />
-        </div>
-        <h1 className="text-center text-[28px] font-medium text-neutral-50">
-          {dict.mainPage.title}
-        </h1>
-        <div className="mt-10 flex max-w-md flex-col items-center gap-5">
-          <StyledLink href={`/${params.lang}/login`}>
-            {dict.mainPage.btnLogin}
-          </StyledLink>
-          <StyledLink href={`/${params.lang}/signup`}>
-            {dict.mainPage.btnSignUp}
-          </StyledLink>
-        </div>
+    <div className="container mx-auto px-6">
+      <ToggleLang />
+      <div className="inline-flex w-full items-center justify-center px-4 py-20">
+        <Image src={tereveniLogo} alt="Tereveni Logo" />
+      </div>
+      <h1 className="text-center text-[28px] font-medium text-neutral-50">
+        {dict.mainPage.title}
+      </h1>
+      <div className="mx-auto mt-10 flex max-w-md flex-col gap-5">
+        <StyledLink href={`/${params.lang}/login`}>
+          {dict.mainPage.btnLogin}
+        </StyledLink>
+        <StyledLink href={`/${params.lang}/signup`}>
+          {dict.mainPage.btnSignUp}
+        </StyledLink>
       </div>
     </div>
   );
