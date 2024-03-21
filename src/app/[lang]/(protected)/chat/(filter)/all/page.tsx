178a3fd61f2fc3e8b74a752d475aeb158ca23ccc
Pavlo Karas <thepavlokaras@gmail.com>
2024-03-21 21:14:11 +0200

    feat: empty chat list message

--- src/app/[lang]/(protected)/chat/(filter)/all/page.tsx
+++ src/app/[lang]/(protected)/chat/(filter)/all/page.tsx
@@ -1,3 +1,4 @@
+import { EmptyChatListMessage } from "@/components/chat/EmptyChatListMessage";
 import { ChatList } from "./chatList";
 import { getChatRooms } from "./chatRooms";
 
@@ -6,6 +7,15 @@ export default async function AllChats({ params }: { params: { lang: Lang } }) {
 
   if (chatRooms.err) {
     return <div>{chatRooms.err}</div>;
+  }
+
+  if (chatRooms.ok?.length === 0) {
+    // Here I calculate the height of a chat list to center the message vertically
+    return (
+      <div className="flex h-[calc(100dvh-75px-92px-40px)] flex-col justify-center px-6">
+        <EmptyChatListMessage lang={params.lang} />
+      </div>
+    );
   }
 
   return (
