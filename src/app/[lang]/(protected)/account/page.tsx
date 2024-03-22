b3d75dbe14be490804488a058a94043156451374
Pavlo Karas <thepavlokaras@gmail.com>
2024-03-22 21:52:07 +0200

    feat: render other menu items

--- src/app/[lang]/(protected)/account/page.tsx
+++ src/app/[lang]/(protected)/account/page.tsx
@@ -1,48 +1,63 @@
 import { ChatListHeader } from "@/components/chat/ChatListHeader";
 import UserProfile from "public/account/user-profile-02.svg";
-import ChevronRight from "public/account/chevron-right.svg";
-import Image from "next/image";
-import Link from "next/link";
-import { ReactNode } from "react";
+import AccountSettings from "public/account/settings.svg";
+import File from "public/account/file-05.svg";
+import { MenuItem } from "./MenuItem";
+import { getDictionary } from "../../dictionaries";
 
-export default function Account({ params }: { params: { lang: Lang } }) {
+export default async function Account({ params }: { params: { lang: Lang } }) {
+  const dict = await getDictionary(`/${params.lang}/account`);
   return (
     <>
       <ChatListHeader lang={params.lang} />
       <main className="px-6 py-10">
-        <div>
+        <div className="flex flex-col gap-3">
           <MenuItem
             href={`/${params.lang}/account/profile`}
-            img={{ src: UserProfile, alt: "User profile" }}
+            img={{ src: UserProfile, alt: dict.myProfile }}
           >
-            My profile
+            {dict.myProfile}
+          </MenuItem>
+          <MenuItem
+            href={`/${params.lang}/account/account-settings`}
+            img={{ src: AccountSettings, alt: dict.accountSettings }}
+          >
+            {dict.accountSettings}
+          </MenuItem>
+          <MenuItem
+            href={`/${params.lang}/terms-and-conditions`}
+            img={{ src: File, alt: dict.termsConditions }}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {dict.termsConditions}
+          </MenuItem>
+          <MenuItem
+            href={`/${params.lang}/privacy-policy`}
+            img={{ src: File, alt: dict.privacyPolicy }}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {dict.privacyPolicy}
+          </MenuItem>
+          <MenuItem
+            href={`/${params.lang}/cookies-policy`}
+            img={{ src: File, alt: dict.cookiesPolicy }}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {dict.cookiesPolicy}
+          </MenuItem>
+          <MenuItem
+            href={`/${params.lang}/community-rules`}
+            img={{ src: File, alt: dict.communityRules }}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {dict.communityRules}
           </MenuItem>
         </div>
       </main>
     </>
-  );
-}
-
-function MenuItem({
-  children,
-  href,
-  img,
-}: {
-  children: ReactNode;
-  href: string;
-  img: {
-    src: any;
-    alt: string;
-  };
-}) {
-  return (
-    <Link
-      href={href}
-      className="flex items-center gap-3 rounded-[32px] border border-[#3E205D] p-4"
-    >
-      <Image src={img.src} alt={img.alt} />
-      <span className="grow">{children}</span>
-      <Image src={ChevronRight} alt="Chevron right" />
-    </Link>
   );
 }
