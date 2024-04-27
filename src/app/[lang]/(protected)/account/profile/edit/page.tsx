d26169e464edd5de0f9593b7a99e9b9ec2c0bdcc
Pavlo Karas <thepavlokaras@gmail.com>
2024-04-27 20:35:48 +0300

    feat: update profile image

--- src/app/[lang]/(protected)/account/profile/edit/page.tsx
+++ src/app/[lang]/(protected)/account/profile/edit/page.tsx
@@ -1,8 +1,7 @@
 import { getDictionary } from "@/app/[lang]/dictionaries";
 import { ProfileHeader } from "./ProfileHeader";
 import { EditProfileForm } from "./EditProfileForm";
-import { env } from "@/env.mjs";
-import { getJwtAccessToken } from "../../../regenerateAccessToken";
+import { fetchUserData } from "./userData";
 
 export default async function EditProfile(props: { params: { lang: Lang } }) {
   const dict = await getDictionary(
@@ -13,33 +12,7 @@ export default async function EditProfile(props: { params: { lang: Lang } }) {
   return (
     <main className="flex w-full flex-col gap-10 px-6 py-10">
       <ProfileHeader lang={props.params.lang} dict={dict} />
-      <EditProfileForm dict={dict} />
+      <EditProfileForm dict={dict} userData={userData} />
     </main>
   );
 }
-
-export async function fetchUserData() {
-  const jwtAccessToken = await getJwtAccessToken();
-
-  const res = await fetch(`${env.SERVER_URL}/api/user/to-edit`, {
-    headers: {
-      Authorization: `Bearer ${jwtAccessToken}`,
-    },
-  });
-
-  return (await res.json()) as UserToEdit;
-}
-
-type UserToEdit = {
-  user: {
-    uuid: string;
-    name: string;
-    userLogin: string;
-    about: string;
-    email: string;
-    image: { name: string };
-    dateLastVisit: null;
-    dateOfCreated: string;
-  };
-  avatars: string[];
-};
