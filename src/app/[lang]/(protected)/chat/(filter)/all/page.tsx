21cfa5ba70784fd16c3c5ff0bc4d3240bbda6c98
Pavlo Karas <thepavlokaras@gmail.com>
2024-06-07 22:11:01 +0300

    feat: internationalization

--- src/app/[lang]/(protected)/chat/(filter)/all/page.tsx
+++ src/app/[lang]/(protected)/chat/(filter)/all/page.tsx
@@ -1,8 +1,10 @@
 import { EmptyChatListMessage } from "@/components/chat/EmptyChatListMessage";
 import { ChatList } from "./chatList";
 import { getChatRooms } from "./chatRooms";
+import { getDictionary } from "@/app/[lang]/dictionaries";
 
 export default async function AllChats({ params }: { params: { lang: Lang } }) {
+  const dict = await getDictionary(`/${params.lang}/chat/all`);
   const chatRooms = await getChatRooms();
 
   if (chatRooms.err) {
@@ -19,7 +21,7 @@ export default async function AllChats({ params }: { params: { lang: Lang } }) {
 
   return (
     <div className="flex flex-col gap-5 px-6">
-      <ChatList chatList={chatRooms.ok!} lang={params.lang} />
+      <ChatList chatList={chatRooms.ok!} lang={params.lang} dict={dict} />
     </div>
   );
 }
