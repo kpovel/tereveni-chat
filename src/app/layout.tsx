b4fffd53081ab6547ecf04207de75e808c61a3a3
Pavlo Karas <thepavlokaras@gmail.com>
2024-06-18 16:57:36 +0300

    feat: meta description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,11 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Create Next App",
+  title: "Tereveni",
+  description:
+    "The Web Chat Application is designed to facilitate real-time communication between users over the Internet",
+  keywords: ["chat", "blog"],
+  applicationName: "Tereveni",
 };
 
 export default function RootLayout({
