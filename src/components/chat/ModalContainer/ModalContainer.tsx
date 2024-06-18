a699fe1615a35225da070b55d7fc47fc0b7eab88
Pavlo Karas <thepavlokaras@gmail.com>
2024-06-18 14:38:18 +0300

    fix: center modal

--- src/components/chat/ModalContainer/ModalContainer.tsx
+++ src/components/chat/ModalContainer/ModalContainer.tsx
@@ -32,12 +32,9 @@ export default function ModalContainer({
   }
 
   return (
-    <div className="absolute left-0 top-0 z-50 h-dvh w-full  bg-opacity-50 bg-cover bg-fixed bg-center bg-no-repeat backdrop-blur-sm backdrop-filter">
-      <div className="relative h-full">
-        <div
-          ref={ref}
-          className="absolute left-2/4 top-2/4 h-[159px] w-[280px] -translate-x-1/2 -translate-y-[150px] transform rounded-lg bg-[#FAFAFA] p-6"
-        >
+    <div className="absolute left-0 top-0 h-full w-full bg-opacity-50 backdrop-blur-sm">
+      <div className="flex h-full items-center justify-center">
+        <div ref={ref}>
           {modalType === "ConfirmationDeleting" && (
             <ConfirmationDeleting
               chatRoomUuid={chatRoomUuid}
